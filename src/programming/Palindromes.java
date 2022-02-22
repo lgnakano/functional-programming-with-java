4b35658ece619ade21310396df7bcd662dfedef9
Luis Nakano <lgnakano@gmail.com>
2022-02-22 12:36:12 -0500

    Simplify functions

--- src/programming/Palindromes.java
+++ src/programming/Palindromes.java
@@ -7,6 +7,12 @@ import java.util.stream.Collectors;
 
 public class Palindromes {
 
+
+    /**
+     *
+     * @param s - input string
+     * @return string with characters sorted
+     */
     private static String transform(String s) {
         return s.chars()
                 .sorted()
@@ -17,19 +23,40 @@ public class Palindromes {
                 .toString();
     }
 
-    private static Map<String, List<String>> getPalindromesMap(List<String> inputStrings) {
+    /**
+     *
+     * @param inputStrings - List of strings
+     * @return map of transformed string to all palindromes on input string
+     */
+    private static Map<String, List<String>> getPalindromeMap(List<String> inputStrings) {
         return inputStrings.stream()
-                .collect(Collectors.groupingBy(Palindromes::transform))
+                .collect(Collectors.groupingBy(Palindromes::transform));
+    }
+    /**
+     *
+     * @param inputStrings - List of strings
+     * @return mapping from transformed string to all palindromes
+     * present on the input strings that have more than one palindrome.
+     */
+    private static Map<String, List<String>> getPalindromesMapFiltered(List<String> inputStrings) {
+        return getPalindromeMap(inputStrings)
                 .entrySet()
                 .stream()
                 .filter(map -> map.getValue().size() >= 2)
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
-    private static List<List<String>> findPalindromes(List<String> inputStrings) {
+    /**
+     *
+     * @param inputStrings - input strings
+     * @return List of Lists of Strings where each list has all
+     * strings on the inputStrings that are palindromes of the string
+     * on that particular position on the list.
+     */
+    private static List<List<String>> findPalindromesList(List<String> inputStrings) {
         return inputStrings.stream()
                 .map(
-                        s -> getPalindromesMap(inputStrings)
+                        s -> getPalindromesMapFiltered(inputStrings)
                                 .getOrDefault(transform(s), new ArrayList<>())
                                 .stream()
                                 .filter( l -> s.compareTo(l) != 0)
@@ -44,8 +71,12 @@ public class Palindromes {
         var inputStrings = List.of(
                 "aab", "aac", "baa", "aad", "eco", "coe", "aba"
         );
+        System.out.println("Input: " + inputStrings);
 
-        System.out.println(findPalindromes(inputStrings));
+        System.out.println(findPalindromesList(inputStrings));
+
+        findPalindromesList(inputStrings)
+                .forEach(System.out::println);
     }
 
 
