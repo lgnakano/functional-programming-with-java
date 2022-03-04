ce82325dddab9d3344df105f9c18cde400083cd0
Luis Nakano <lgnakano@gmail.com>
2022-03-04 07:24:29 -0500

    Check if IP address is in the network.

---
+++ src/programming/NetworkMask.java
@@ -0,0 +1,42 @@
+package programming;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class NetworkMask {
+
+     public static void main(String[] args) {
+        String network = "192.168.1.128/25";
+        String ip = "192.168.1.127";
+
+        System.out.println(binary(ip).substring(0, 25));
+        System.out.println(binary(ip).substring(0, 25).length());
+
+        System.out.println(ipInNetwork(ip, network));
+    }
+
+    private static boolean ipInNetwork(String ip, String network) {
+
+        String[] networkElements = network.split("/");
+        int maskLength = Integer.parseInt(networkElements[1]);
+        String ipBinary = binary(ip)
+                .substring(0, maskLength);
+        String networkBinary = binary(networkElements[0])
+                .substring(0, maskLength);
+
+        return ipBinary.compareTo(networkBinary) == 0;
+    }
+
+
+    private static String binary(String ip) {
+       return  Arrays.stream(ip.split("\\."))
+                .map((String s) ->
+                        String.format("%8s",Integer.toBinaryString(
+                            Integer.parseInt(s)))
+                        .replaceAll(" ", "0")
+                )
+                .collect(
+                        Collectors.joining(""));
+
+    }
+}
